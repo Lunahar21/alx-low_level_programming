cf789109f1cbb50e44561c29a9ff5694db07b963
“Lunahar21” <kindinewd@gmail.com>
2023-02-16 12:45:21 -0800

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-print a line of my code
+ *
+ * return:o
+ */
+  int main(void)
+    {
+	    puts("\"Programming is like building a multilingual puzzle");
+	    return(0);
+    }
