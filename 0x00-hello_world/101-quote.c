482b555ebea18d1d3ca0a6dea56880d2d8bd9974
“Lunahar21” <kindinewd@gmail.com>
2023-02-16 15:55:36 -0800

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main - A c programming that prints the standared error
- * coded by Kindinew
- * Return: 1 (success)
+ * main - A c programming that prints the standared errorw
+ * Return 1 (success)
  */
 
 int main(void)
 {
-write(2, "and that piece of art is useful" - Dora Korpar, 2015 - 10 - 19\n", 59);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19/n*, stdout);
 return (1);
 }
