352c5ef59522feba273394c4a06207cd9cffdb1b
“Lunahar21” <kindinewd@gmail.com>
2023-02-18 06:51:40 -0800

    print_combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * description print all possible combination of single digit numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c = '0';
+	int i;
+
+	while (c <= '9')
+	{
+		i = c + 1;
+		while (i <= '9')
+		{
+		putchar(c);
+		putchar(i);
+	if (c != '8' || (c == '8' && i != '9'))
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	i++;
+	}
+	c++;
+	}
+	putchar('\n');
+	return (0);
+}
