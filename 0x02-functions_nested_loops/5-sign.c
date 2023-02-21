d79a83d5249af3c4296a7f79d9a04c82b8da74ce
Kindinew Dessie Assefa <kindinewd@gmail.com>
2023-02-22 01:52:28 +0300

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,11 +1,10 @@
 #include "main.h"
-/**
- * print_sign - Prints the sign of a number.
- * @n: The number of which the sign will be printed.
- * Return: if n is greaterthan 0 return 1 and If n is lessthan 0 return -1,
- * if it its equal 0 return 0.
-*/
 
+/**
+* print_sign -> prints sign based on condition
+* @n: argument passed
+* Return: 1, 0 -1
+*/
 int print_sign(int n)
 {
 	if (n > 0)
@@ -13,16 +12,15 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else
+	else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
-Footer
-© 2023 GitHub, Inc.
+
