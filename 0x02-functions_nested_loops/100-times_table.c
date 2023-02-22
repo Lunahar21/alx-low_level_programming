203e46c7b9572c4ba373820768f87a8d4f265bd0
“Lunahar21” <kindinewd@gmail.com>
2023-02-22 13:01:00 -0800

    print_time tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * print - times table - prints the n times table, starting with 0
- * @n: The integer to print the times table for.
+ * @n:If n is greater than 15 or less than 0 the function should not print
  */
 void print_times_table(int n)
 {
