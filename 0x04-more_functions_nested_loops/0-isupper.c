2754a68e0193cfc113badc74f2f90eb8a7bd38ac
“Lunahar21” <kindinewd@gmail.com>
2023-02-23 13:58:42 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - checks for uppercase character
+ * @c: is upper case character
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+int _isupper(int c);
+{
+	char c;
+
+	for (c >= 'A' && c <= 'Z')
+
+		return (1);
+		while
+			return (0)
+}
