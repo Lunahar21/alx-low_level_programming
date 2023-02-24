221978252538cb72ec7c571c5df63888b434fa86
“Lunahar21” <kindinewd@gmail.com>
2023-02-24 00:07:59 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - is adigit to be checked
+ * @c: a digit of 0 to 9 checked
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c);
+{
+	if (c >= '0' && c <= '9')
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
