8c35810e3e83bab2ead45278429ca69fa6deb702
“Lunahar21” <kindinewd@gmail.com>
2023-02-24 06:15:51 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * more_numbers - print 10 times the numbers 
+ * more_numbers - no return
+ * Bwave ICT file
  *
- * Return: void
- */
+ * Return: no return
+*/
 
 void more_numbers(void)
 {
-	int j, k;
+	int i, j;
 
-	for (j = 0; j <= 10; j++)
+	for (i = 1; i <= 10; i++)
+
 	{
+		for (j = 0; j <= 14; j++)
 
-		for (k = 0; k <= 14; k++)
 		{
-			if (k >= 10)
-	
-				 _putchar('1');
-			_putchar((k % 10 + '0');
-	}
-	_putchar('\n');
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
+		}
+
+		_putchar('\n');
 	}
 }
