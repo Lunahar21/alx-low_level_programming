d1e8cc1690cb1b411ada69e6c4751f7681dcb041
“Lunahar21” <kindinewd@gmail.com>
2023-02-24 01:20:24 -0800

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print numbres 0 to 9 except 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char a;
+
+	for (a = '0'; a <= '9'; c++)
+	{
+		if (!(c == '2' || c == '4'))
+			_putchar(a);
+	}
+	_putchar('\n');
+}
