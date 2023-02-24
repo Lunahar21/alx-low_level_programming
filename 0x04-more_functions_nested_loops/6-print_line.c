868f4004748f4c5c453136cf0690a374be97ecc8
“Lunahar21” <kindinewd@gmail.com>
2023-02-24 03:22:05 -0800

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,19 @@
 
 void print_line(int n)
 {
-		int i;
+		int j;
 
-		for (i = 0; i <= n; i++)
+		if (n <= 0)
+		{
+		_putchar('\n');
+		}
+		else
+		{
+
+		for (j = 0; j <= n; j++)
 		{
 			_putchar(_);
 		}
 		_putchar('\n');
+		}
 }
