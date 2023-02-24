806900ce66adbdb0ddf81ff8309b2daabf74e72a
“Lunahar21” <kindinewd@gmail.com>
2023-02-24 01:24:23 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,9 +9,9 @@ void print_most_numbers(void)
 {
 	char a;
 
-	for (a = '0'; a <= '9'; c++)
+	for (a = '0'; a <= '9'; a++)
 	{
-		if (!(c == '2' || c == '4'))
+		if (!(a == '2' || a == '4'))
 			_putchar(a);
 	}
 	_putchar('\n');
