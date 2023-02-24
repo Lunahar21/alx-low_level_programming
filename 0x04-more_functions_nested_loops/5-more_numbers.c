c675026c6b9c55e21c4bba25092eaa85e6d3b2f4
“Lunahar21” <kindinewd@gmail.com>
2023-02-24 06:07:35 -0800

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,23 +3,23 @@
 /**
  * more_numbers - print 10 times the numbers since 0 up to 14
  *
- * Return: 10 times of the numbers since 0 to 14
+ * Return: no 
  */
 
 void more_numbers(void)
 {
 	int j, k;
 
-	for (j = 0; j < 10; j++)
+	for (j = 0; j <= 10; j++)
 	{
 
 	for (k = 0; k <= 14; k++)
 	{
-	if (k > 9)
+	if (k >= 10)
 	{
-	 _putchar((k / 10) + '0');
+	 _putchar('1');
 	}
-	_putchar((k % 10) + '0');
+	_putchar((k % 10 + '0');
 	}
 	_putchar('\n');
 	}
