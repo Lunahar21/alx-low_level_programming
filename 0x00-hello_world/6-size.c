bb1604f8d99e601cf2463735efa725c9695aaa58
“Lunahar21” <kindinewd@gmail.com>
2023-02-25 07:16:08 -0800

    size data type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,7 @@
 int main(void)
 
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
+	char a; int b; long int c; long long int d; float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of int: %lu byte(s)\n", (unsigned long)sizeof(b));
@@ -21,4 +17,4 @@ printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
 return (0);
-}}
+}
