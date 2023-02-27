38a61050c3bd0edf6b2f3d57ae1393a2d12c15d2
“Lunahar21” <kindinewd@gmail.com>
2023-02-27 06:13:21 -0800

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: length of the string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int j = 0;
+
+	while (s[j])
+		j++;
+
+	return (length);
+}
