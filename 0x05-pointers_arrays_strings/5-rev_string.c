c0a16234691b4241d6dd1695981e5a3396dad2f1
“Lunahar21” <kindinewd@gmail.com>
2023-02-27 12:42:37 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+
+{
+	int i, len, temp;
+
+	len = strlen(str1);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		temp = str1[i];
+		str1[i] = str1[len - i - 1];
+		str1[len - i - 1] = temp;
+	}
+}
