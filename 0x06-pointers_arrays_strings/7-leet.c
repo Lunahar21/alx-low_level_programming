7d02d5733776ae9a851f84542ba31da936e77501
“Lunahar21” <kindinewd@gmail.com>
2023-03-02 11:21:23 -0800

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,21 +9,19 @@
 
 char *leet(char *sr)
 {
-	int i, j;
-	char *ltr = "aAeEoOtTlL";
-	char *num = "4433007711";
+	int index, j;
+	char lower[] = {'a', 'e', '0', 't', 'l', '\0'};
+	char numbs[] = {'4', '3', '0', '7', '1', '\0'};
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (index = 0; sr[index] != '\0'; index++)
 	{
-		for (j = 0; ltr[j] != '\0'; j++)
+		for (j = 0; j < 5; j++)
 		{
-			if (str[i] == ltr[j])
+			if (sr[index] == lower[j] || sr[indx] == upper[j])
 			{
-				str[i] = num[j];
-				break;
+				sr[index] = numbs[j];
 			}
 		}
 	}
-
-	return (str);
+	return (sr);
 }
