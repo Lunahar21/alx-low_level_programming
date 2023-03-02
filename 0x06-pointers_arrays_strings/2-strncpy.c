1e92244fd7d0507893ba7cf7541e07352888be76
“Lunahar21” <kindinewd@gmail.com>
2023-03-02 02:35:11 -0800

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int index;
 
-	for (index = 0; index < n; src[index] != '\0'; index++)
+	for (index = 0; index < n && src[index] != '\0'; index++)
 	{
 		dest[index] = src[index];
 	}
-	for (; index < n; index++)
+		for (; index < n; index++)
 		{
 			dest[index] = '\0';
 		}
