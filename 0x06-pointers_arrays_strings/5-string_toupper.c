ee208bb79f7c7efdb36989ee792997979cfa0a01
“Lunahar21” <kindinewd@gmail.com>
2023-03-02 08:42:27 -0800

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,17 +4,18 @@
  * *string_toupper - a function that changes all lowercase letter to uppercase
  *
  * @n: input string
- * Return: lowercase to upercase
+ * Return: a pointer to string changed
  */
 
 char *string_toupper(char *n)
 {
 	int index;
 
+
 	for (index = 0; n[index] != '\0'; index++)
 	{
 		if (n[index] >= 'a' && n[index] <= 'z')
-		
+
 			n[index] -= 32;
 		}
 	return (n);
