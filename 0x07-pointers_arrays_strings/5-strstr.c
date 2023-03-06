2760bbd394c0e40dd3a4ac91588160f8702a94d2
“Lunahar21” <kindinewd@gmail.com>
2023-03-06 08:42:42 -0800

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -4,38 +4,24 @@
 /**
  * *_strstr - a function that locates a substring
  *@haystack: input string
- *@needle: input string
- *
- *Return: 0
+ *@needle: input
+ * Return: 0
  */
 char *_strstr(char *haystack, char *needle)
 {
-	for (; *haystack != '\0'; haystack++)
+	while (*haystack)
 	{
-<<<<<<< HEAD
-		char *k = haystack;
-		char *m = needle
+		char *k;
 
-
-		while (*k == *m && *m != '\0')
-=======
-		char *kin;
-
-		kin = haystack;
+		k = haystack;
 		while (*needle == *haystack)
->>>>>>> 91eef4091f8d913e0f73316a5215050494b09db8
 		{
-			k++;
-			m++;
+			haystack++;
+			needle++;
 		}
-<<<<<<< HEAD
-		if (*m == '\0')
-			return (haystack);
-=======
 		if (*needle == '\0')
-			return (kin);
+			return (k);
 		haystack++;
->>>>>>> 91eef4091f8d913e0f73316a5215050494b09db8
 	}
 	return (0);
 }
