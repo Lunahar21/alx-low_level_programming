eada0c1a2900320f7b272d005892fb5d59b1c337
“Lunahar21” <kindinewd@gmail.com>
2023-03-06 07:21:07 -0800

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,4 +9,16 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	return (s)
+	int j;
+
+	while (*s)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (*s == accept[j])
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
