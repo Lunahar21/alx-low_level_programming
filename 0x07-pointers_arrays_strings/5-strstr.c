91eef4091f8d913e0f73316a5215050494b09db8
Kindinew Dessie Assefa <123399151+Lunahar21@users.noreply.github.com>
2023-03-06 19:07:28 +0300

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *_strstr - a function that locates a substring
@@ -11,15 +12,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	while (*haystack)
 	{
-		char *k;
+		char *kin;
 
-		k = haystack;
+		kin = haystack;
 		while (*needle == *haystack)
 		{
 			haystack++, needle++;
 		}
 		if (*needle == '\0')
-			return (k);
+			return (kin);
 		haystack++;
 	}
 	return (0);
