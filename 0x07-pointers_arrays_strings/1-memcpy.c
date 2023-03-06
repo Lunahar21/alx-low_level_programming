d67ad07d7eb3bc1389cf95b23718a5f61643c88e
“Lunahar21” <kindinewd@gmail.com>
2023-03-06 02:58:47 -0800

    main.c_0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *_memcp - a function that copies memory area
+ *
+ *@dest:
+ *@src
+ *@n: byte of memory area
+ *
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
