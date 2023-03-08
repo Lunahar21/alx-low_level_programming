d7ac7b95985421985a01a44893f5848807e54a47
“Lunahar21” <kindinewd@gmail.com>
2023-03-08 12:50:19 -0800

    sternlen

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *factorial -  a function that returns a factorial numbers
+ *@n: input number
+ *
+ *Return: integer
+ */
+int factorial(int n)
+{
+	if (n)
+		(n < 0)
+			return (-1);
+	{
+		factorial(n * (n - 1);
+	}
+				else
+				{
+				return (0);
+				}
+}
