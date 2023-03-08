7bbc648295b9efb9dfa40d6795c0af3f44d37e37
“Lunahar21” <kindinewd@gmail.com>
2023-03-08 15:10:53 -0800

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,17 +2,17 @@
 
 /**
  *is_prime_number - a function that returns 1 if input prime else 0
- *@n: prime input number
+ *@prime: prime input number
  *
  *Return: 1 if prime number else 0
  */
-int is_prime_number(int n)
+int is_prime_number(int prime)
 {
-	if (n <= 1)
+	if (prime <= 1)
 		return (0);
-	if (n == 2)
+	if (prime == 2)
 		return (1);
-	if ((n % 2 ==) || (n % 3 == 0) || (n % 5 == 0))
+	if ((prime % 2 == 0) || (prime % 3 == 0) || (prime % 5 == 0))
 		return (0);
 	else
 		return (1);
