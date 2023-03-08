4afdac67af744005c8299281186524b9aba170f1
“Lunahar21” <kindinewd@gmail.com>
2023-03-08 14:49:37 -0800

    square recurtion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,11 +11,11 @@ int k_sqrt_recursion(int p, int m)
 		if (n == (m * m))
 			return  (m);
 
-	else if ((m * m) >= n)
+	else if (m == p / 2)
 		return (-1);
 
 	else
-		return (k_sqrt_recursion(n, m + 1));
+		return (k_sqrt_recursion(p, m + 1));
 }
 /**
  *_sqrt_recursion - the first function
@@ -25,7 +25,10 @@ int k_sqrt_recursion(int p, int m)
  */
 int _sqrt_recursion(int n)
 {
-	if (n <= 0)
+	if (n < 0)
 		return (-1);
+	else if (n == 0 || n == 1)
+		return (n);
+	else
 	return (k_sqrt_recursion(n, 0));
 }
