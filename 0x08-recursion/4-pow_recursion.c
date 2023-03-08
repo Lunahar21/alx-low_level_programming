96a8bbe9d43ded3d81b0c2e162a0501b0685c469
“Lunahar21” <kindinewd@gmail.com>
2023-03-08 14:02:55 -0800

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,22 @@
 #include "main.h"
 
 /**
+ *_pow_recursion - a function that returns the value of x raised to y
  *
+ *@x: input
+ *@y: input
  *
- *
- *
- *
- *
+ * Return: integer
  */
-int _sqrt_recursion(int n)
+int _pow_recursion(int x, int y)
 {
+	if (y == 1)
 
+		return (x);
+
+	else if (n % 2 == 0)
+
+		return (_pow_recursion(_pow_recursion(x, y / 2), 2));
+	else
+		return (x * _pow_recursion(x, y - 1));
 }
-
