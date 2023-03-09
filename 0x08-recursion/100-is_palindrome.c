03645a2ddf0def703f85cadd500d0e19365f71b6
“Lunahar21” <kindinewd@gmail.com>
2023-03-09 06:42:22 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,25 +1,55 @@
 #include "main.h"
+
 /**
- *is_palindrome - a function that return 1 if str plaindrome else 0
+ * get_string_length - finds the length of a string recursively
  *
- *@s: a pointer pointing to char
+ * @str: the string to find the length of
  *
- *Return: 1 or 0
+ * Return: the length of the string
  */
-int is_palindrome(char *s)
+int get_string_length(char *str)
 {
-	char s;
-	int i, length;
-
-	length = strlen(s);
-
-	for (i = 0; i < length; i++)
-	{
-		if (s[i] == (s[length - i - 1])
-			return (1);
-
-	}
-	else
+	if (*str == '\0')
 		return (0);
+
+	return (1 + get_string_length(str + 1));
 }
+
+/**
+ * is_palindrome_helper - checks if a string is a palindrome recursively
+ *
+ * @str: the string to check
+ * @len: the length of the string
+ * @index: the current index being checked
+ *
+ * Return: 1 if the string is a palindrome, otherwise 0
+ */
+int is_palindrome_helper(char *str, int len, int index)
+{
+	if (index >= len / 2)
+		return (1);
+
+	if (str[index] == str[len - index - 1])
+		return (is_palindrome_helper(str, len, index + 1));
+
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ *
+ * @str: the string to check
+ *
+ * Return: 1 if the string is a palindrome, otherwise 0
+ */
+int is_palindrome(char *str)
+{
+	int len = get_string_length(str);
+
+	if (len <= 1)
+		return (1);
+
+	return (is_palindrome_helper(str, len, 0));
+}
+
 
