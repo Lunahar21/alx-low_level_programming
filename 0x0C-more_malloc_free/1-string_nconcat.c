966ba8b00e3c76c60e1abc01f32dda3a234113a3
“Lunahar21” <kindinewd@gmail.com>
2023-03-16 12:02:32 -0700

    string_concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
-
 /**
- **string_nconcat - a function thatconcatenate 2 strings
+ **string_nconcat -  a function that concatenates two strings
  *@s1: destination string
- *@s2: source string
- *@n: size s2 copied to s1 string
+ *@s2: copied string to s1
+ *@n: size of copied sring s2
+ *
  *Return: string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
@@ -13,29 +13,32 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int a, b;
 	char *newString;
 
-
 	if (s1 == NULL)
 		s1 = "";
-	if (s == NULL)
+	if (s2 == NULL)
 		s2 = "";
 	for (a = 0; s1[a] != '\0'; a++)
-		;
+	{
+
+	}
 	for (b = 0; s2[b] != '\0'; b++)
-		;
+	{
+
+	}
 	if (n > b)
 		n = b;
-	newString = malloc(((a + n) + 1));
-	if (newString == NULL)
-		return (NULL);
+	newString[a] = malloc(((a + n) + 1))
+		if (newString == NULL)
+			return (NULL);
 	for (a = 0; s1[a] != '\0'; a++)
 	{
 		newString[a] = s1[a];
 	}
-	for (b = 0; s2[b] != '\0'; b++)
+	for (b = 0; b != n; b++)
 	{
 		newString[a] = s2[b];
-		a++
+		a++;
 	}
-	newString[a] = '\0';
+	newString[a] = s2[b];
 	return (newString);
 }
