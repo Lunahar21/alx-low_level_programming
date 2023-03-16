b150e4f3d3d768db03bfbf31c81a07fcbeda166f
“Lunahar21” <kindinewd@gmail.com>
2023-03-16 11:40:01 -0700

    string-concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,18 +13,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int a, b;
 	char *newString;
 
+
 	if (s1 == NULL)
 		s1 = "";
-	if (s2 == NULL)
+	if (s == NULL)
 		s2 = "";
 	for (a = 0; s1[a] != '\0'; a++)
-	{
-
-	}
+		;
 	for (b = 0; s2[b] != '\0'; b++)
-	{
-
-	}
+		;
 	if (n > b)
 		n = b;
 	newString = malloc(((a + n) + 1));
@@ -37,12 +34,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (b = 0; s2[b] != '\0'; b++)
 	{
 		newString[a] = s2[b];
-		a++;
+		a++
 	}
 	newString[a] = '\0';
 	return (newString);
 }
-
-
-
-
