311aafd5adbf3d7de626234a5b015a5aaf81d52a
“Lunahar21” <kindinewd@gmail.com>
2023-03-20 13:24:10 -0700

    wusha

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
-#include "dog.h"
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * init_dog - Initializes a variable of type struct dog.
@@ -11,10 +10,11 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
 	}
 }
+#endif
