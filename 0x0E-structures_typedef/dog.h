0372599456f1a2318bdc9497f779da02178d9f3f
“Lunahar21” <kindinewd@gmail.com>
2023-03-20 12:20:51 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,13 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 /**
  *init_dog - initialze a variable of type struct dog
- *@: pointer to the adress to initialize
+ *@d: pointer to the adress to initialize
  *@name: the dog's name
  *@age: the dog's age
  *@owner: the owner's name
+ * Return: nothing
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
