50c5b957ecce7c09bde4eda3e122c45e9bd2b2ef
“Lunahar21” <kindinewd@gmail.com>
2023-03-21 12:03:40 -0700

    free_new_dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,18 +1,15 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
- *free_dog - free the new dogs
- *@d: struct to be allocated
- *
- *Return: void
+ * free_dog - free new dog
+ * @d: struct to be deallocate
  */
 void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-	{
-		free(d->name);
-		free(d->owner);
-		free(d);
-	}
+
+	free(d->name);
+	free(d->owner);
+	free(d);
 }
