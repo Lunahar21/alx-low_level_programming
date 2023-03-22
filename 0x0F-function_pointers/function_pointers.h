a7a1f561534361cb98f964b78f01e21a2e21acec
“Lunahar21” <kindinewd@gmail.com>
2023-03-22 12:07:09 -0700

    name_print

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
-#ifndef FUNCTION_POINTER_H
-#define FUNCTION_POINTER_H
+#ifndef _FUNCTION_POINTER_
+#define _FUNCTION_POINTER_
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdio.h>
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
