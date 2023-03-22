0c30b636ae2984e038fe92dde828cff59f6ca69f
“Lunahar21” <kindinewd@gmail.com>
2023-03-22 12:26:34 -0700

    array_iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - function  excutes a function
+ *@array: array of integers
+ *@size: size of array
+ *@action: fuction pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
