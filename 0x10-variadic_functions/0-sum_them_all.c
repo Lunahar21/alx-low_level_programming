f8893534dbf28c706f6627ced13a77725786df87
“Lunahar21” <kindinewd@gmail.com>
2023-03-23 12:05:15 -0700

    sum_variadic

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,18 +11,19 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 
-	va_start(args, n);
-
 	unsigned int i;
 
 	int sum = 0;
+	int x;
+
+	va_start(args, n);
 
 	if (n == 0)
 		return (0);
 
 	for (i = 0; i < n; i++)
 	{
-		int x = va_arg(args, unsigned int);
+		x = va_arg(args, int);
 
 		sum = sum + x;
 	}
